0cd3354792b38facfd1ef3ccce0c0d3abdd5603b
Mich <50410305+MikeCoder96@users.noreply.github.com>
2024-07-06 16:56:21 +0200

    Dark mode, resume&pause, filtering, UI Adjustment

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 // main.dart
 
 import 'dart:io';
+import 'dart:async'; // Add this import
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:http/http.dart' as http;
 import 'package:qbitsmart/settings.dart';
 import 'package:qbitsmart/torrent.dart';
@@ -51,6 +53,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        /* light theme settings */
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        /* dark theme settings */
+      ),
+      themeMode: ThemeMode.dark,
+      /* ThemeMode.system to follow system theme,
+         ThemeMode.light for light theme,
+         ThemeMode.dark for dark theme
+      */
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(connectionInfo: connectionInfo),
     );
   }
@@ -67,7 +83,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String _cookie = '';
+  String _selectedFilter = 'All';
   List<Torrent> _torrentList = [];
+  Timer? _timer; // Declare a Timer
 
   @override
   void initState() {
@@ -76,6 +94,48 @@ class _MyHomePageState extends State<MyHomePage> {
       await loginToQBitTorrent(widget.connectionInfo);
       await getTorrentList(widget.connectionInfo);
     });
+
+    // Initialize the timer to refresh every second
+    _timer = Timer.periodic(Duration(seconds: 1), (Timer t) => getTorrentList(widget.connectionInfo));
+  }
+
+
+  List<Torrent> _filteredTorrentList(String filter){
+
+    if (filter == "Active")
+      return _torrentList
+          .where((torrent) => torrent.state == "downloading" || torrent.state == "uploading")
+          .toList();
+
+    if (filter == "Downloading")
+      return _torrentList
+          .where((torrent) => (torrent.state == "downloading"))
+          .toList();
+
+    if (filter == "Seeding")
+      return _torrentList
+          .where((torrent) => (torrent.state == "uploading" || torrent.state == "queuedUP"|| torrent.state == "stalledUP"))
+          .toList();
+
+    if (filter == "Completed")
+      return _torrentList
+          .where((torrent) => (torrent.state == "completed" || torrent.state == "uploading" || torrent.state == "queuedUP"|| torrent.state == "stalledUP"))
+          .toList();
+
+    if (filter == "Errored")
+      return _torrentList
+          .where((torrent) => (torrent.state == "errored"))
+          .toList();
+
+
+    return _torrentList;
+
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel(); // Cancel the timer when the widget is disposed
+    super.dispose();
   }
 
   @override
@@ -100,18 +160,53 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
-            _createDrawerItem(icon: Icons.arrow_upward, text: 'All'),
-            _createDrawerItem(icon: Icons.arrow_downward, text: 'Downloading'),
-            _createDrawerItem(icon: Icons.check, text: 'Completed'),
-            _createDrawerItem(icon: Icons.upload, text: 'Seeding'),
-            _createDrawerItem(icon: Icons.pause, text: 'Paused'),
-            _createDrawerItem(icon: Icons.flash_on, text: 'Active'),
-            _createDrawerItem(icon: Icons.flash_off, text: 'Inactive'),
+            _createDrawerItem(icon: Icons.filter_list, text: 'All (${_filteredTorrentList("All").length})', onTap: () {
+              setState(() {
+                _selectedFilter = 'All';
+              });
+              Navigator.pop(context);
+            },),
+            _createDrawerItem(icon: Icons.flash_on, text: 'Active (${_filteredTorrentList("Active").length})', onTap: () {
+              setState(() {
+                _selectedFilter = 'Active';
+              });
+              Navigator.pop(context);
+            },),
+            _createDrawerItem(icon: Icons.download, text: 'Downloading (${_filteredTorrentList("Downloading").length})', onTap: () {
+              setState(() {
+                _selectedFilter = 'Downloading';
+              });
+              Navigator.pop(context);
+            },),
+            _createDrawerItem(icon: Icons.upload, text: 'Seeding (${_filteredTorrentList("Seeding").length})', onTap: () {
+              setState(() {
+                _selectedFilter = 'Seeding';
+              });
+              Navigator.pop(context);
+            },),
+            _createDrawerItem(icon: Icons.pause, text: 'Paused (${_filteredTorrentList("Paused").length})', onTap: () {
+              setState(() {
+                _selectedFilter = 'Paused';
+              });
+              Navigator.pop(context);
+            },),
+            _createDrawerItem(icon: Icons.check, text: 'Completed (${_filteredTorrentList("Completed").length})', onTap: () {
+              setState(() {
+                _selectedFilter = 'Completed';
+              });
+              Navigator.pop(context);
+            },),
+            _createDrawerItem(icon: Icons.error, text: 'Errored (${_filteredTorrentList("Errored").length})', onTap: () {
+              setState(() {
+                _selectedFilter = 'Errored';
+              });
+              Navigator.pop(context);
+            },),
+
             Divider(),
             _createDrawerItem(icon: Icons.settings, text: 'Settings', onTap: () {
               Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsPage(connectionInfo: widget.connectionInfo)));
-              }),
-            _createDrawerItem(icon: Icons.upgrade, text: 'Get Pro'),
+            }),
             _createDrawerItem(icon: Icons.help, text: 'Help'),
           ],
         ),
@@ -120,56 +215,69 @@ class _MyHomePageState extends State<MyHomePage> {
         child: _torrentList.isEmpty
             ? CircularProgressIndicator()
             : ListView.builder(
-          itemCount: _torrentList.length,
+          itemCount: _filteredTorrentList(_selectedFilter).length,
           itemBuilder: (BuildContext context, int index) {
+            final torrent = _filteredTorrentList(_selectedFilter)[index];
             return GestureDetector(
               onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => TorrentDetailsPage(torrent: _torrentList[index]),
+                    builder: (context) => TorrentDetailsPage(torrent: torrent),
                   ),
                 );
               },
               child: Card(
                 child: ListTile(
-                  title: Text(_torrentList[index].name),
+                  title: Text(torrent.name),
                   subtitle: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('${_torrentList[index].category}'),
-                      Row(
-                        children: [
-                          Text(
-                            'Progress: ',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(width: 8),
-                          Expanded(
-                            child: LinearProgressIndicator(
-                              value: double.parse(_torrentList[index].progress),
+                      Text('${torrent.category}'),
+                      if (torrent.state == "downloading")...[
+                        Row(
+                          children: [
+                            Text(
+                              'Progress: ',
+                              style: TextStyle(fontWeight: FontWeight.bold),
                             ),
-                          ),
-                          SizedBox(width: 8),
-                          Text('${(double.parse(_torrentList[index].progress) * 100).toStringAsFixed(1)}%'),
-                        ],
-                      ),
-                      Text('Size: ${(_torrentList[index].size / 1024 / 1024 / 1024).toStringAsFixed(2)} GB / '
-                          '${(_torrentList[index].completed / 1024 / 1024 / 1024).toStringAsFixed(2)} GB'),
-                      Text('Ratio: ${_torrentList[index].ratio.toStringAsFixed(2)}'),
+                            SizedBox(width: 8),
+                            Expanded(
+                              child: LinearProgressIndicator(
+                                value: double.parse(torrent.progress),
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                            Text('${(double.parse(torrent.progress) * 100).toStringAsFixed(1)}%'),
+                          ],
+                        ),
+                        Text('Download: ${(torrent.downloadSpeed / 1024 / 1024).toStringAsFixed(2)} MB/s'),
+                        Text('Upload: ${(torrent.uploadSpeed / 1024 / 1024).toStringAsFixed(2)} MB/s'),
+                      ],
+
+
+                      Text('Size: ${(torrent.size / 1024 / 1024 / 1024).toStringAsFixed(2)} GB / '
+                          '${(torrent.completed / 1024 / 1024 / 1024).toStringAsFixed(2)} GB'),
+                      Text('(${((torrent.size * torrent.ratio)/ 1024 / 1024 / 1024).toStringAsFixed(2)}) Ratio: ${torrent.ratio.toStringAsFixed(2)}'),
                     ],
                   ),
                   trailing: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      IconButton(
-                        icon: Icon(Icons.pause),
-                        onPressed: () => pauseTorrent(_torrentList[index].hash),
-                      ),
+                      if (torrent.state == "downloading" || torrent.state == "uploading" || torrent.state == "queuedUP" || torrent.state == "queuedDL" || torrent.state == "stalledUP" || torrent.state == "stalledDL")
+                        IconButton(
+                          icon: Icon(Icons.pause),
+                          onPressed: () => pauseTorrent(torrent.hash),
+                        ),
+                      if (torrent.state == "pausedDL" || torrent.state == "pausedUP")
+                        IconButton(
+                          icon: Icon(Icons.play_arrow),
+                          onPressed: () => resumeTorrent(torrent.hash),
+                        ),
                       IconButton(
                         icon: Icon(Icons.delete),
-                        onPressed: () => deleteTorrent(_torrentList[index].hash, true),
+                        onPressed: () => deleteTorrent(torrent.hash, true),
                       ),
                     ],
                   ),
@@ -179,23 +287,43 @@ class _MyHomePageState extends State<MyHomePage> {
           },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => AddTorrentPage(cookie: _cookie, connectionInfo: widget.connectionInfo),
-            ),
-          );
-        },
-        child: const Icon(Icons.add),
+      floatingActionButton: SpeedDial(
+          icon: Icons.add,
+          children: [
+            SpeedDialChild(
+              child: Icon(Icons.link),
+              onTap: null,),
+            SpeedDialChild(
+              child: Icon(Icons.file_open),
+              onTap: null,),
+          ]
       ),
     );
   }
 
-
   Future<void> pauseTorrent(String hash) async {
     final url = '${widget.connectionInfo.useHttps ? 'https' : 'http'}://${widget.connectionInfo.ipAddress}:${widget.connectionInfo.port}${widget.connectionInfo.path}/api/v2/torrents/pause';
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Cookie': 'SID=$_cookie',
+        },
+        body: {
+          'hashes': hash,
+        },
+      );
+
+      if (response.statusCode != 200) {
+        _showErrorDialog('Failed to pause torrent: ${response.statusCode}');
+      }
+    } catch (e) {
+      _showErrorDialog('Error pausing torrent: $e');
+    }
+  }
+
+  Future<void> resumeTorrent(String hash) async {
+    final url = '${widget.connectionInfo.useHttps ? 'https' : 'http'}://${widget.connectionInfo.ipAddress}:${widget.connectionInfo.port}${widget.connectionInfo.path}/api/v2/torrents/resume';
     try {
       final response = await http.post(
         Uri.parse(url),
@@ -286,6 +414,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _showErrorDialog('Error logging in: $e');
     }
   }
+
   Widget _createDrawerItem(
       {required IconData icon, required String text, GestureTapCallback? onTap}) {
     return ListTile(
